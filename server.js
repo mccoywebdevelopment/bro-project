d6c52f6eaba44f40a820f34fb7533fb3bcf62712
Christopher <cmmccoy2@asu.edu>
2018-11-05 19:24:55 -0700

    Trying to add mongoDB to proj.

--- server.js
+++ server.js
@@ -1,9 +1,32 @@
-var http=require('http');
-var express=require('express');
-var bodyParser=require('body-parser');
+const http=require('http');
+const express=require('express');
+const bodyParser=require('body-parser');
 const app=express();
-open = require('opn'),
+const fs = require('fs');
+const MongoClient = require('mongodb').MongoClient;
+const Users=require('./models/users');
+ 
+var contents = fs.readFileSync('keys.txt', 'utf8');
+console.log(contents);
 
+var db
+MongoClient.Promise=global.Promise;
+MongoClient.connect(contents, (err, client) => {
+  if (err) return console.log(err)
+  db = client.db('bro_proj') // whatever your database name is
+});
+
+app.get('/users',function(req,res){
+  Users.find({})
+  .exec(function(err,Users){
+    if(err){
+      console.log(err);
+    }
+    else{
+      console.log(Users);
+    }
+  });
+});
 app.use(bodyParser.urlencoded({extended:true}));
 
 app.use( express.static( "public" ) );
@@ -13,6 +36,6 @@ app.get('/',function(req,res){
 })
 app.listen(2000,function() {
     console.log("Listening on Port:"+2000);
-    open('http://localhost:2000');
+    //open('http://localhost:2000');
 });
 //lksdsdf
